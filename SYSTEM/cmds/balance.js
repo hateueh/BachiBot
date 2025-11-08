7f6b41eb56795604843cb3e7be5bc3661df12359
hateueh <crgff7959@gmail.com>
2025-11-08 13:15:50 +0300

    balance.js

--- SYSTEM/cmds/balance.js
+++ SYSTEM/cmds/balance.js
@@ -1,42 +1,57 @@
-module.exports = {
-	config: {
-		name: "رصيدي",
-		aliases: ["bal"],
-		version: "1.2",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-		
-			ar: "عرض رصيدك الحالي أو رصيد الأعضاء"
-		},
-		category: "إقتصاد",
-	guide: {
-			
-			ar: "   {pn}: عرض الأموال"
-				+ "\n   {pn} <@منشن>: قم برؤية رصيد الشخص اللذي قمت بعمل منشن له"
-		}
-	},
+const fs = require("fs");
 
-	langs: {
-		
-		ar: {
-			money: "رصيدك هو 『 %1 』دولار 💵",
-			moneyOf: "『رصيد %1 هو 『 %2 دولار 💵"
-		}
-	},
+module.exports = {  
+	config: {  
+		name: "رصيدي",  
+		aliases: ["bal"],  
+		version: "1.3",  
+		author: "NTKhang + عبودي & ChatGPT 🎀",  
+		countDown: 5,  
+		role: 0,  
+		description: {  
+			ar: "عرض رصيدك الحالي أو رصيد الأعضاء"  
+		},  
+		category: "إقتصاد",  
+		guide: {  
+			ar: "   {pn}: عرض الأموال"  
+				+ "\n   {pn} <@منشن>: قم برؤية رصيد الشخص الذي قمت بعمل منشن له"  
+		}  
+	},  
+  
+	langs: {  
+		ar: {  
+			money: "رصيدك هو 『 %1 』 دولار 💵",  
+			moneyOf: "رصيد 『 %1 』 هو 『 %2 دولار 💵",  
+			adminRich: "يااااه يا ادمن 😭💸 تم إضافة مليون دولار جديدة لرصيدك! رصيدك الحالي الآن 『 %1 دولار 💵』"
+		}  
+	},  
+  
+	onStart: async function ({ message, usersData, event, getLang }) {  
+		const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+		const adminIDs = config.adminBot || [];
 
-	onStart: async function ({ message, usersData, event, getLang }) {
-		if (Object.keys(event.mentions).length > 0) {
-			const uids = Object.keys(event.mentions);
-			let msg = "";
-			for (const uid of uids) {
-				const userMoney = await usersData.get(uid, "money");
-				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
-			}
-			return message.reply(msg);
+		// لو في منشن
+		if (Object.keys(event.mentions).length > 0) {  
+			const uids = Object.keys(event.mentions);  
+			let msg = "";  
+			for (const uid of uids) {  
+				const userMoney = await usersData.get(uid, "money");  
+				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + "\n";  
+			}  
+			return message.reply(msg);  
+		}  
+
+		const userID = event.senderID;  
+		let userData = await usersData.get(userID);  
+
+		// لو المستخدم من الأدمنز
+		if (adminIDs.includes(userID)) {
+			userData.money = (userData.money || 0) + 1000000;
+			await usersData.set(userID, { money: userData.money });
+			return message.reply(getLang("adminRich", userData.money));
 		}
-		const userData = await usersData.get(event.senderID);
-		message.reply(getLang("money", userData.money));
-	}
+
+		// عادي للباقي
+		message.reply(getLang("money", userData.money));  
+	}  
 };
